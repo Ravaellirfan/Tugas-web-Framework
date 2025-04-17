001db3a390ebe60f7b8913711fa071161e0104d6
Ravaellirfan <ravaell38aye@gmail.com>
2025-04-17 13:10:35 +0700

    first commit

---
+++ application/views/home/tentang.php
@@ -0,0 +1,139 @@
+<section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url(); ?>assets/User/images/bg_2.jpg');">
+	<div class="overlay"></div>
+	<div class="container">
+		<div class="row no-gutters slider-text align-items-center justify-content-center">
+			<div class="col-md-9 ftco-animate text-center">
+				<h1 class="mb-2 bread">Tentang Kami</h1>
+				<p class="breadcrumbs"><span class="mr-2"><a href="#">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Tentang<i class="ion-ios-arrow-forward"></i></span></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="ftco-section ftco-no-pt ftc-no-pb">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 order-md-last wrap-about py-5 wrap-about bg-light">
+				<div class="text px-4 ftco-animate">
+					<h2 class="mb-4">Selamat datang di B'Smart Tulang Bawang Barat</h2>
+					<p>B’Smart Rumah Belajar adalah sebuah lembaga pendidikan non formal yang berada di bawah naungan Yayasan Prima Bintang Bunda. Berdiri pada bulan Desember tahun 2007 di Surabaya.</p>
+					<p>Lembaga ini berkonsentrasi pada program-program belajar yang ditujukan untuk mengembangkan serta mengoptimalkan potensi kecerdasan anak. Diharapkan keberadaan B’Smart dapat menjadi mitra terbaik bagi orang tua dan sekolah dalam program pengembangan kecerdasan anak.</p>
+					<p><a href="#" class="btn btn-secondary px-4 py-3">Read More</a></p>
+				</div>
+			</div>
+			<div class="col-md-7 wrap-about py-5 pr-md-4 ftco-animate">
+				<h2 class="mb-4">Apa yang kita tawarkan</h2>
+				<p>Yayasan Prima Bintang Bunda telah membuat program belajar bagi anak usia 3-8 tahun yang dirancang untuk mengasah ketrampilan berpikir tingkat tinggi (high order thinking skill) tersebut. Khusus bagi anak-anak berusia 4-6 tahun.</p>
+				<div class="row mt-5">
+					<div class="col-lg-6">
+						<div class="services-2 d-flex">
+							<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-security"></span></div>
+							<div class="text">
+								<h3>Keselamatan</h3>
+								<p>Point ini sangat lah kami jaga karna keselamatan adalah nomor satu dari bimble kami.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="services-2 d-flex">
+							<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-reading"></span></div>
+							<div class="text">
+								<h3>Kategori kelas</h3>
+								<p>Khusus bagi anak-anak berusia 4-6 tahun, tersedia program CintaBACA dan CintaMATIKA.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="services-2 d-flex">
+							<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-diploma"></span></div>
+							<div class="text">
+								<h3>Guru Bersertifikat</h3>
+								<p>Semua Guru yang mengajar bersertifikat resmi.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="services-2 d-flex">
+							<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-education"></span></div>
+							<div class="text">
+								<h3>Ruang Kelas yang Cukup</h3>
+								<p>Terdapat 8 ruang kelas.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="services-2 d-flex">
+							<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-jigsaw"></span></div>
+							<div class="text">
+								<h3>Pelajaran Kreatif</h3>
+								<p>Terdapat Pembelajaran dengan metode yang menyenangkan.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6">
+						<div class="services-2 d-flex">
+							<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-kids"></span></div>
+							<div class="text">
+								<h3>Fasilitas olahraga</h3>
+								<p>Ada fasilitas olahraga berupa lapangan basket dan lapangan futsal</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(<?= base_url(); ?>assets/User/images/bg_4.jpg);" data-stellar-background-ratio="0.5">
+	<div class="container">
+		<div class="row justify-content-center mb-5 pb-2">
+			<div class="col-md-8 text-center heading-section heading-section-black ftco-animate">
+				<h2 class="mb-4"><span>13 Tahun</span> Pengalaman</h2>
+				<p>Memiliki guru guru yang berbakat pada bidangnya masing - masing dengaan pengalaman yang dimiliki guru akan membuat kelas menjadi seru dan menyenangkan.</p>
+			</div>
+		</div>
+		<div class="row d-md-flex align-items-center justify-content-center">
+			<div class="col-lg-10">
+				<div class="row d-md-flex align-items-center">
+					<div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+						<div class="block-18">
+							<div class="icon"><span class="flaticon-doctor"></span></div>
+							<div class="text">
+								<strong class="number" data-number="<?= $total_admin; ?>">0</strong>
+								<span>Total Admin</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+						<div class="block-18">
+							<div class="icon"><span class="flaticon-doctor"></span></div>
+							<div class="text">
+								<strong class="number" data-number="<?= $total_pendaftar; ?>">0</strong>
+								<span>Total Pendaftar</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+						<div class="block-18">
+							<div class="icon"><span class="flaticon-doctor"></span></div>
+							<div class="text">
+								<strong class="number" data-number="<?= $total_user; ?>">0</strong>
+								<span>Total Siswa</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+						<div class="block-18">
+							<div class="icon"><span class="flaticon-doctor"></span></div>
+							<div class="text">
+								<strong class="number" data-number="<?= $total_kelas; ?>">0</strong>
+								<span>Total Kelas</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
